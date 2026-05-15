fc1db4ca920d7ad9eec0b682df7736adedafcabb
Lua <luru0001@stud.ek.dk>
2026-05-15 12:28:14 +0200

    comit-alot

--- javascript/menu.js
+++ javascript/menu.js
@@ -2,10 +2,12 @@ const burgerBtn = document.querySelector(".burger-btn");
 const closeBtn = document.querySelector(".menu-close");
 const header = document.querySelector(".header");
 
+/* Åbn menu */
 burgerBtn.addEventListener("click", () => {
   header.classList.add("menu-open");
 });
 
+/* Luk menu */
 closeBtn.addEventListener("click", () => {
   header.classList.remove("menu-open");
 });
